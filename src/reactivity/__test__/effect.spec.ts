68de3a05ca50750f2ad02e565733133febcdea30
pzy1 <782046126@qq.com>
2022-05-27 01:04:29 +0800

    effect收集对应的依赖,也只触发对应的副作用函数

--- src/reactivity/__test__/effect.spec.ts
+++ src/reactivity/__test__/effect.spec.ts
@@ -21,16 +21,19 @@ test("effect and reactive基本使用", () => {
 //         num1: 1,
 //         num2: 1
 //     })
-//     let num = 0
+//     let num1,num2 = 0
 //     effect(() => {
-//         console.log(obj.num1)
-//         num++
+//         num1 = obj.num1
+//         num2++
 //     })
-//     expect(num).toBe(1)
+//     expect(num1).toBe(1)
+//     expect(num2).toBe(1)
 //     obj.num1++
-//     expect(num).toBe(2)
+//     expect(num1).toBe(2)
+//     expect(num2).toBe(2)
 //     obj.num2++ // 这一次设置obj.num2不应该触发副作用函数的重新执行
-//     expect(num).toBe(2)
+//     expect(num1).toBe(2)
+//     expect(num2).toBe(3)
 // })
 
 test("effect收集对应的依赖,也只触发对应的副作用函数", () => {
@@ -38,14 +41,18 @@ test("effect收集对应的依赖,也只触发对应的副作用函数", () => {
         num1: 1,
         num2: 1
     })
-    let num
+    let num = 0
     const mockFn = jest.fn(() => {
+        console.log('mockFn call')
         num = obj.num1
     })
     effect(mockFn)
     expect(mockFn).toHaveBeenCalledTimes(1)
-    obj.num1++
+    expect(num).toBe(1)
+    obj.num1 = 2
     expect(mockFn).toHaveBeenCalledTimes(2)
-    obj.num2++ // 这一次设置obj.num2不应该触发副作用函数的重新执行
-    expect(mockFn).toHaveBeenCalledTimes(2)
+    expect(num).toBe(2)
+    obj.num2=3 // 这一次设置obj.num2不应该触发副作用函数的重新执行
+    expect(mockFn).toHaveBeenCalledTimes(2) // 副作用函数依旧只执行2次
+    expect(num).toBe(2)
 })
--- src/reactivity/reactive.ts
+++ src/reactivity/reactive.ts
@@ -1,15 +1,24 @@
 import { activeEffect } from "./effect"
 
 // 存副作用的容器
-const bucket:Set<Function> = new Set()
+const bucket:WeakMap<any,Map<any,Set<Function>>> = new WeakMap()
 
 export function reactive(data){
     const obj = new Proxy(data, {
         // 拦截读取操作
         get(target, key){
+            let depsMap = bucket.get(target)
+            if(!depsMap){
+                bucket.set(target, (depsMap = new Map()))
+            }
+            let deps = depsMap.get(key)
+            if(!deps){
+                depsMap.set(key, (deps = new Set()))
+            }
+
             if(activeEffect){
                 // 如果存在注册的副作用函数，则收集进容器
-                bucket.add(activeEffect)
+                deps.add(activeEffect)
             }
             // 返回属性值
             return target[key]
@@ -18,8 +27,11 @@ export function reactive(data){
         set(target, key, newValue){
             // 设置新属性值
             target[key] = newValue
+            const depsMap = bucket.get(target)
+            if(!depsMap) return false
+            const deps = depsMap.get(key)
             // 从容器内取出所有副作用函数，并执行
-            bucket.forEach(fn => fn())
+            deps && deps.forEach(fn => fn())
             return true
         }
     })
