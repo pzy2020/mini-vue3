640ce2617152d0f355f01b77ce6a3b5bcb6bb369
pzy1 <782046126@qq.com>
2022-06-07 23:56:56 +0800

    挂载渲染嵌套的普通元素、正确的设置元素属性

--- src/runtime-dom/index.ts
+++ src/runtime-dom/index.ts
@@ -10,6 +10,10 @@ function setElementText(el, text) {
     el.textContent = text
 }
 
+function shouldSetAsProps(el,key){
+    return key in el
+}
+
 export function createRenderer(options = {createElement, insert, setElementText}){
     const {
         createElement,
@@ -21,6 +25,23 @@ export function createRenderer(options = {createElement, insert, setElementText}
         const el = createElement(vnode.type)
         if(typeof vnode.children === 'string'){
             setElementText(el,vnode.children)
+        }else if(Array.isArray(vnode.children)){
+            vnode.children.forEach(child => {
+                patch(null, child, el)
+            })
+        }
+        if(vnode.props){
+            for (const key in vnode.props) {
+                const value = vnode.props[key]
+                if(shouldSetAsProps(el,key)){
+                    const type = typeof el[key]
+                    if(type === 'boolean' && value === ''){
+                        el[key] = true
+                    }else{
+                        el[key] = value
+                    }
+                }
+            }
         }
         insert(el,container)
     }
--- src/runtime-dom/tests/index.spec.ts
+++ src/runtime-dom/tests/index.spec.ts
@@ -2,6 +2,14 @@ import { createRenderer } from '../index'
 import { ref } from '../../reactivity/ref'
 import { effect } from '../../reactivity/effect'
 
+afterEach(()=>{
+    let appDom = document.querySelector('#app')
+    if(appDom){
+        let parent = appDom.parentNode
+        parent && parent.removeChild(appDom)
+    }
+})
+
 test("挂载渲染普通的简单元素", () => {
     let text = 'hello vue3'
     let dom = document.createElement('div')
@@ -14,4 +22,64 @@ test("挂载渲染普通的简单元素", () => {
     let renderer = createRenderer()
     renderer.render(vnode, document.querySelector('#app'))
     expect(dom.querySelector('p')?.innerHTML).toBe(text)
+})
+
+test("挂载渲染嵌套的普通元素", () => {
+    let text = 'hello vue3'
+    let dom = document.createElement('div')
+    dom.setAttribute('id', 'app')
+    document.body.append(dom)
+    const vnode = {
+        type: 'div',
+        children: [
+            {
+                type: 'p',
+                children: text
+            }
+        ]
+    }
+    let renderer = createRenderer()
+    renderer.render(vnode, document.querySelector('#app'))
+    expect(dom.querySelector('p')?.innerHTML).toBe(text)
+})
+
+test("普通的简单元素更新", () => {
+    let text = ref('hello vue3')
+    let dom = document.createElement('div')
+    dom.setAttribute('id', 'app')
+    document.body.append(dom)
+    const vnode = {
+        type: 'p',
+        children: text.value
+    }
+    let renderer = createRenderer()
+    let textVal
+    effect(() => {
+        textVal = text.value
+        renderer.render(vnode, document.querySelector('#app'))
+    })
+    
+    expect(dom.querySelector('p')?.innerHTML).toBe(text.value)
+    text.value = 'vue3 hello'
+    expect(textVal).toBe(text.value)
+    expect(dom.querySelector('p')?.innerHTML).toBe(text.value)
+})
+
+test("正确的设置元素属性:disabled", () => {
+    let dom = document.createElement('div')
+    dom.setAttribute('id', 'app')
+    document.body.append(dom)
+    const vnode = {
+        type: 'button',
+        props:{
+            disabled: '',
+            id: 'submit'
+        },
+        children: 'Submit'
+    }
+    let renderer = createRenderer()
+    renderer.render(vnode, document.querySelector('#app'))
+    let submitDom = document.querySelector('button')
+    expect(submitDom?.innerHTML).toBe('Submit')
+    expect(submitDom?.disabled).toBe(true)
 })
