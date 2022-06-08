85e364801370411346d1a7f7a09ee7563fa654db
pzy1 <782046126@qq.com>
2022-06-09 00:29:36 +0800

    render卸载元素

--- src/runtime-dom/index.ts
+++ src/runtime-dom/index.ts
@@ -10,19 +10,86 @@ function setElementText(el, text) {
     el.textContent = text
 }
 
+function parentNode(child:Element | null){
+    return child?.parentNode
+}
+
+function remove(child) {
+    const parent = parentNode(child)
+    if(parent){
+        parent.removeChild(child)
+    }
+}
+
+function patchProp(el, key, preVal, nextVal) {
+    if(key === 'style'){
+        // 遍历新的style对象直接覆盖
+        for (const k in nextVal) {
+            el.style[k] = nextVal[k]
+        }
+        // 遍历旧的style对象，把不存在于新的对象中的样式属性置空
+        for (const k in preVal) {
+            if(!(k in nextVal)){
+                el.style[k] = null
+            }
+        }
+    }else if(key === 'class'){ 
+        el.className = nextVal
+    }else if(shouldSetAsProps(el,key)){
+        const type = typeof el[key]
+        if(type === 'boolean' && nextVal === ''){
+            el[key] = true
+        }else{
+            el[key] = nextVal
+        }
+    }else{
+        el.setAttribute(key, nextVal)
+    }
+}
+
 function shouldSetAsProps(el,key){
     return key in el
 }
 
-export function createRenderer(options = {createElement, insert, setElementText}){
+export function createRenderer(options = {createElement, insert, setElementText, remove}){
     const {
         createElement,
         insert,
-        setElementText
+        setElementText,
+        remove
     } = options
 
+    function patchChildren(n1, n2, container){
+        if(typeof n2.children === 'string'){
+            setElementText(container, n2.children)
+        }
+    }
+
+    function unmount(vnode){
+        remove(vnode.el)
+    }
+
+    function patchElement(n1, n2){
+        const el = n2.el = n1.el
+        const oldProps = n1.props
+        const newProps = n2.props
+        for (const key in newProps) {
+            if(newProps[key] !== oldProps[key]){
+                patchProp(el, key, oldProps[key], newProps[key])
+            }
+        }
+
+        for (const key in oldProps) {
+            if(!(key in newProps)){
+                patchProp(el, key, oldProps[key], null)
+            }
+        }
+
+        patchChildren(n1, n2, el)
+    }
+
     function mountElement(vnode, container){
-        const el = createElement(vnode.type)
+        const el = vnode.el = createElement(vnode.type)
         if(typeof vnode.children === 'string'){
             setElementText(el,vnode.children)
         }else if(Array.isArray(vnode.children)){
@@ -32,32 +99,38 @@ export function createRenderer(options = {createElement, insert, setElementText}
         }
         if(vnode.props){
             for (const key in vnode.props) {
-                const value = vnode.props[key]
-                if(shouldSetAsProps(el,key)){
-                    const type = typeof el[key]
-                    if(type === 'boolean' && value === ''){
-                        el[key] = true
-                    }else{
-                        el[key] = value
-                    }
-                }
+                const nextVal = vnode.props[key]
+                patchProp(el, key, null, nextVal)
             }
         }
         insert(el,container)
     }
 
     function patch(n1, n2, container){
-        if(!n1){
-            mountElement(n2, container)
+        if(n1 === n2) return
+        if(n1 && n1.type !== n2.type){
+            unmount(n1)
+            n1 = null
         }
+        const { type } = n2
+        if(typeof type === 'string'){
+            if(!n1){
+                mountElement(n2, container)
+            }else{
+                console.log(n1,n2)
+                patchElement(n1, n2)
+            }
+        }
+        
     }
 
     function render(vnode, container){
+        console.log(container._vnode,vnode)
         if(vnode){ // 挂载
             patch(container._vnode, vnode, container)
         }else { // 卸载
             if(container._vnode){
-                container.innerHTML = ''
+                unmount(container._vnode)
             }
         }
         container._vnode = vnode
--- src/runtime-dom/tests/index.spec.ts
+++ src/runtime-dom/tests/index.spec.ts
@@ -48,15 +48,14 @@ test("普通的简单元素更新", () => {
     let dom = document.createElement('div')
     dom.setAttribute('id', 'app')
     document.body.append(dom)
-    const vnode = {
-        type: 'p',
-        children: text.value
-    }
     let renderer = createRenderer()
     let textVal
     effect(() => {
         textVal = text.value
-        renderer.render(vnode, document.querySelector('#app'))
+        renderer.render({
+            type: 'p',
+            children: text.value
+        }, document.querySelector('#app'))
     })
     
     expect(dom.querySelector('p')?.innerHTML).toBe(text.value)
@@ -82,4 +81,60 @@ test("正确的设置元素属性:disabled", () => {
     let submitDom = document.querySelector('button')
     expect(submitDom?.innerHTML).toBe('Submit')
     expect(submitDom?.disabled).toBe(true)
+})
+
+test("正确的设置元素属性:class", () => {
+    let dom = document.createElement('div')
+    dom.setAttribute('id', 'app')
+    document.body.append(dom)
+    const vnode = {
+        type: 'button',
+        props:{
+            id: 'submit',
+            class: 'error'
+        },
+        children: 'Submit'
+    }
+    let renderer = createRenderer()
+    renderer.render(vnode, document.querySelector('#app'))
+    let submitDom = document.querySelector('button')
+    expect(submitDom?.innerHTML).toBe('Submit')
+    expect(submitDom?.className).toBe('error')
+})
+
+test("正确的设置元素属性:style", () => {
+    let dom = document.createElement('div')
+    dom.setAttribute('id', 'app')
+    document.body.append(dom)
+    const vnode = {
+        type: 'button',
+        props:{
+            id: 'submit',
+            class: 'error',
+            style: {color: 'rgb(0, 0, 0)'}
+        },
+        children: 'Submit'
+    }
+    let renderer = createRenderer()
+    renderer.render(vnode, document.querySelector('#app'))
+    let submitDom:HTMLButtonElement|null = document.querySelector('#submit')
+    expect(submitDom?.style.color).toBe('rgb(0, 0, 0)')
+    expect(submitDom?.className).toBe('error')
+})
+
+test("render方法卸载元素", () => {
+    let text = 'hello vue3'
+    let dom = document.createElement('div')
+    dom.setAttribute('id', 'app')
+    document.body.append(dom)
+    const vnode = {
+        type: 'p',
+        children: text
+    }
+    let renderer = createRenderer()
+    renderer.render(vnode, document.querySelector('#app'))
+    expect(dom.querySelector('p')?.innerHTML).toBe(text)
+    renderer.render(null, document.querySelector('#app'))
+    expect(dom.querySelector('p')).toBe(null)
+    expect(document.querySelector('#app')?.innerHTML).toBe('')
 })
